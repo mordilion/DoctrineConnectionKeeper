39dde217974413029aa9cea44c4bb61687be8aa9
Henning <henning.huncke@check24.de>
2021-04-20 16:19:10 +0200

    add missing namespace

--- src/Mordilion/Doctrine/DBAL/Connections/PrimaryReadReplicaConnection.php
+++ src/Mordilion/Doctrine/DBAL/Connections/PrimaryReadReplicaConnection.php
@@ -11,6 +11,8 @@
 
 declare(strict_types=1);
 
+namespace Mordilion\Doctrine\DBAL\Connections;
+
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Connections\PrimaryReadReplicaConnection as DBALPrimaryReadReplicaConnectionAlias;
 use Doctrine\DBAL;
